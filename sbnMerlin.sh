2d570c6fd9bf46fbee7e9736aecebf0524538e39
crazy-matt <crazy-matt.noreply.xjsdh@tenet.pm>
2026-02-15 12:53:45 +0000

    fix: change target branch to combined-changes one

--- sbnMerlin.sh
+++ sbnMerlin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 readonly script_name="sbnMerlin"
-readonly script_branch="bounce-eth-clients"
+readonly script_branch="combined-changes"
 readonly script_repo="https://raw.githubusercontent.com/crazy-matt/$script_name/$script_branch"
 
 /usr/sbin/curl -fsL --retry 3 "$script_repo/$script_name.sh" -o "/jffs/scripts/$script_name"
